f2215146f4c8db8256acce5a939e204f620cd5a8
beck <bolun365@163.com>
2022-03-12 23:05:17 +0800

    u

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "New_codes",
+        "excerpt":"codes  code  ","categories": ["codes"],
+        "tags": [],
+        "url": "/codes/new_codes/",
+        "teaser": "/assets/basic/homepage.jpg"
+      },{
+        "title": "Newcompetions",
+        "excerpt":"competions  内容  ","categories": ["competions"],
+        "tags": [],
+        "url": "/competions/newcompetions/",
+        "teaser": "/assets/basic/teaser_default.jpg"
+      },{
+        "title": "New_projects",
+        "excerpt":"projects  内容  ","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/new_projects/",
+        "teaser": "/assets/basic/teaser_default.jpg"
+      },{
+        "title": "Publications",
+        "excerpt":"publications  内容  ","categories": ["publications"],
+        "tags": [],
+        "url": "/publications/publications/",
+        "teaser": "/assets/basic/teaser_default.jpg"
+      },{
+        "title": "Newmath",
+        "excerpt":"math  ","categories": ["math"],
+        "tags": [],
+        "url": "/math/newmath/",
+        "teaser": "/assets/basic/teaser_default.jpg"
+      }]
---
+++ publish.sh
@@ -0,0 +1,5 @@
+bundle exec jekyll build
+cd _site
+git add *
+git commit -m"u"
+git push origin
