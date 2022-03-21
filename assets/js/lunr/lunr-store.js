f241705f97c2762703ecdb68902f951b91d13219
Your Name <you@example.com>
2022-03-21 15:41:45 +0800

    u

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -24,7 +24,7 @@ var store = [{
         "teaser": "/assets/basic/teaser_default.jpg"
       },{
         "title": "图片信息统计",
-        "excerpt":"统计单通道 def get_gray_hist(gray_image): # 图片, 使用的通道, mask, HistSize, 直方图柱的范围 hist= cv2.calcHist([gray_image], [0], None, [256], [0.0,255.0]) # 归一化 src, dst, min, max, norm_type cv2.normalize(hist, hist,0,500*0.9,cv2.NORM_MINMAX) # min_val, max_val, min_loc, max_loc = cv.minMaxLoc(hist) hist_img = np.zeros([500,256,3], np.uint8) for h, intensity in enumerate(hist): intensity = int(intensity) cv2.line(hist_img,(h,500), (h,500-intensity), (255,255,255)) return hist_img 统计多通道 ```python...","categories": ["codes"],
+        "excerpt":"统计单通道 def get_gray_hist(gray_image): # 图片, 使用的通道, mask, HistSize, 直方图柱的范围 hist= cv2.calcHist([gray_image], [0], None, [256], [0.0,255.0]) # 归一化 src, dst, min, max, norm_type cv2.normalize(hist, hist,0,500*0.9,cv2.NORM_MINMAX) # min_val, max_val, min_loc, max_loc = cv.minMaxLoc(hist) hist_img = np.zeros([500,256,3], np.uint8) for h, intensity in enumerate(hist): intensity = int(intensity) cv2.line(hist_img,(h,500), (h,500-intensity), (255,255,255)) return hist_img 统计多通道 def...","categories": ["codes"],
         "tags": [],
         "url": "/codes/%E5%9B%BE%E7%89%87%E4%BF%A1%E6%81%AF%E7%BB%9F%E8%AE%A1/",
         "teaser": "/assets/basic/teaser_default.jpg"
@@ -36,7 +36,7 @@ var store = [{
         "teaser": "/assets/basic/teaser_default.jpg"
       },{
         "title": "数据增强",
-        "excerpt":"import imutils import imutils.paths import os import cv2 import numpy as np import random import PIL.Image def random_salt(im, salt_num, diameter): \"\"\" 椒盐噪声 :param im: :param salt_num: :param diameter: 椒盐噪声半径 :return: \"\"\" assert diameter &gt; 1 radius = int(diameter/2) if not diameter % 2: diameter += 1 if im.shape[0] &lt; diameter...","categories": ["codes"],
+        "excerpt":"椒盐噪声 def random_salt(im, salt_num, diameter): \"\"\" 椒盐噪声 :param im: :param salt_num: :param diameter: 椒盐噪声半径 :return: \"\"\" assert diameter &gt; 1 radius = int(diameter/2) if not diameter % 2: diameter += 1 if im.shape[0] &lt; diameter or im.shape[1] &lt; diameter: raise Exception(\"diameter error\") filled_circle = [] circle = [] for i...","categories": ["codes"],
         "tags": [],
         "url": "/codes/%E6%95%B0%E6%8D%AE%E5%A2%9E%E5%BC%BA/",
         "teaser": "/assets/basic/teaser_default.jpg"
